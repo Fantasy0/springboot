f259f41a0391469b09ad480226da10422de6e9db
Fantasy0 <1227702985@qq.com>
2018-11-29 11:22:12 +0800

    swagger整合

--- src/main/java/com/example/mybatisdemo/controller/UserController.java
+++ src/main/java/com/example/mybatisdemo/controller/UserController.java
@@ -5,6 +5,10 @@ import com.example.mybatisdemo.entity.User;
 import com.example.mybatisdemo.service.RedisService;
 import com.example.mybatisdemo.service.UserService;
 import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -19,7 +23,11 @@ public class UserController {
     RedisService redisService;
     @Resource
     UserService userService;
-
+    @ApiOperation(value="查看所有用户", notes="查看所有用户")
+    @ApiImplicitParams({
+            @ApiImplicitParam(dataType = "Integer", name = "pageNum", value = "页数", paramType = "query"),
+            @ApiImplicitParam(dataType = "Integer", name = "pageSize", value = "每页数量", paramType = "query")
+    })
     @GetMapping("/getAll")
     public BaseDTO getAllUser(@RequestParam(required = false ,defaultValue = "1") Integer pageNum,
                               @RequestParam(required = false ,defaultValue = "10") Integer pageSize) {
@@ -33,7 +41,8 @@ public class UserController {
         return BaseDTO.successBaseDTO(userMap);
     }
 
-
+    @ApiOperation(value = "创建用户", notes = "创建用户")
+    @ApiImplicitParam(name = "user" ,value = "用户信息",dataType = "User",required = true)
     @PostMapping("/saveUser")
     public BaseDTO saveUser(@RequestBody User user) {
         try {
