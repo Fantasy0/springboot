30e3824bb9f36f916d48386c646eb4cb04153aa0
Fantasy0 <1227702985@qq.com>
2018-11-30 18:01:36 +0800

    test

---
+++ src/main/java/com/example/mybatisdemo/entity/FastJsonTest.java
@@ -0,0 +1,19 @@
+package com.example.mybatisdemo.entity;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+@Data
+@AllArgsConstructor
+public class FastJsonTest {
+    @JsonIgnore
+    private Integer id;
+
+    private String name;
+    @JSONField(format = "yyyy-mm-dd")
+    private Date date;
+}
